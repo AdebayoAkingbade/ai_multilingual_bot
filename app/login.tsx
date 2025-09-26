547c20364c41f3e2325034c856b7e2b6689b16e2
Akingbacrown <aakingbade@ecobank.com>
2025-09-26 14:39:06 +0100

    first push

---
+++ app/login.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+
+export default function LoginPage(){
+    return(
+        <View>
+            <Text>
+                This is the login Page for you all
+            </Text>
+        </View>
+    )
+}
